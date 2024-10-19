d6966365a083319831571a64a68bd523a35cb8f4
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2024-10-19 12:32:53 +0400

    initial commit but better

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import static org.example.Util.someKindOfFunction;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye world!");
+        someKindOfFunction("THE CHANGE HAPPENED.");
     }
+
+
 }
